f161a9b30015f9c845265bfd8a33014a9aece5a8
JC <cjoaquinangel@gmail.com>
2024-02-28 15:19:14 -0600

    Removed application properties

---
+++ src/main/java/com/project/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("file:/")
+public class AppConfig {
+    
+}
